c6a7b4d5e0d4ca4b70707f30eb9afa410e84ceaa
Rostyk <rostik.durjagin@gmail.com>
2026-04-20 13:55:05 +0300

    fix 5

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ def main() -> None:
     with open("players.json", encoding="utf-8") as file:
         data = json.load(file)
 
-    for nickname, player_data in data.items():
+    for player_data in data.get("players", []):
         race_data = player_data["race"]
         race, _ = Race.objects.get_or_create(
             name=race_data["name"],
-            defaults={"description": race_data.get("description", "")}
+            defaults={"description": race_data.get("description", "")},
         )
 
         guild_data = player_data.get("guild")
@@ -21,15 +21,15 @@ def main() -> None:
         if guild_data:
             guild, _ = Guild.objects.get_or_create(
                 name=guild_data["name"],
-                defaults={"description": guild_data.get("description")}
+                defaults={"description": guild_data.get("description")},
             )
 
         Player.objects.create(
-            nickname=nickname,
+            nickname=player_data["nickname"],
             email=player_data["email"],
             bio=player_data["bio"],
             race=race,
-            guild=guild
+            guild=guild,
         )
 
         for skill_data in race_data.get("skills", []):
@@ -38,7 +38,7 @@ def main() -> None:
                 defaults={
                     "bonus": skill_data.get("bonus", ""),
                     "race": race,
-                }
+                },
             )
 
 
